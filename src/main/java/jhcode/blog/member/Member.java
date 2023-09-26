0c45ab238e5f2e175876649a56f09e02a3dbf469
jhcode33 <jhcode33@gmail.com>
2023-09-26 23:06:44 +0900

    Feat: Add JWT, Change MemberEnitity extends UserDetails
    - JWT 로그인 구현을 위해서 UserDetails를 상속받고, 각 메소드를 구현했습니다

--- src/main/java/jhcode/blog/member/Member.java
+++ src/main/java/jhcode/blog/member/Member.java
@@ -5,10 +5,19 @@ import jhcode.blog.common.BaseTimeEntity;
 import jhcode.blog.common.Role;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Entity
-public class Member extends BaseTimeEntity {
+@NoArgsConstructor // 기본 생성자
+public class Member extends BaseTimeEntity implements UserDetails {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -28,9 +37,7 @@ public class Member extends BaseTimeEntity {
     private Role roles;
 
 
-    //== 생성자 ==//
-    public Member() {}
-
+    //========== 생성자 Builder ============//
     @Builder
     public Member(String email, String password, String username, Role roles) {
         this.email = email;
@@ -39,8 +46,37 @@ public class Member extends BaseTimeEntity {
         this.roles = roles;
     }
 
+    //========== Dirty Checking ===========//
     public void update(String password, String username) {
         this.password = password;
         this.username = username;
+    }
+
+    //========== UserDetails implements ==========//
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add( new SimpleGrantedAuthority("ROLE_" + this.roles.name()));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
--- src/main/java/jhcode/blog/member/MemberRepository.java
+++ src/main/java/jhcode/blog/member/MemberRepository.java
@@ -2,5 +2,9 @@ package jhcode.blog.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
