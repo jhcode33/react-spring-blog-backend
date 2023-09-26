6d1257e4850885fbac4693921023466b415c9c91
jhcode33 <jhcode33@gmail.com>
2023-09-26 23:08:15 +0900

    Feat: Add CustomUserDetailsService
    - CustomUserDetailsService를 구현합니다

---
+++ src/main/java/jhcode/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+package jhcode.blog.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    String resourceName;
+    String fieldName;
+    String fieldValue;
+
+    /**
+     * RuntimeException(message)
+     * @param resourceName : 찾을 수 없는 자원명
+     * @param fieldName : 필드명
+     * @param fieldValue : 필드값
+     */
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s is not found [%s : %s]", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
---
+++ src/main/java/jhcode/blog/security/jwt/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package jhcode.blog.security.jwt;
+
+import jhcode.blog.exception.ResourceNotFoundException;
+import jhcode.blog.member.Member;
+import jhcode.blog.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private MemberRepository memberRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.memberRepo.findByEmail(username).orElseThrow(
+                () -> new ResourceNotFoundException("Member", "Member Email : ", username));
+    }
+}
