feae788db6bf39c51a66cb541138be316898c34a
jhcode33 <jhcode33@gmail.com>
2023-09-26 22:04:32 +0900

    Init: Project Init, Add .gitignore

---
+++ src/main/java/jhcode/blog/ReactSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package jhcode.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jhcode/blog/ReactSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package jhcode.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
